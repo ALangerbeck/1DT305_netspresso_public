a7b85e09ed01f02bc4175aa22c925176d481a7f0
Alfred Langerbeck <32991448+ALangerbeck@users.noreply.github.com>
2023-07-04 14:42:00 +0200

    Update boot.py

--- src/boot.py
+++ src/boot.py
@@ -29,12 +29,12 @@ def do_connect():
 
     wlan = network.WLAN(network.STA_IF)         # Put modem on Station mode
 
-    if not wlan.isconnected():                  # Check if already connected
+    if not wlan.isconnected():                  
         print('connecting to network...')
         wlan.active(True)                       # Activate network interface
         # set power mode to get WiFi power-saving off (if needed)
         wlan.config(pm = 0xa11140)
-        wlan.connect(WIFI_SSID, WIFI_PASSWORD)  # Your WiFi Credential
+        wlan.connect(WIFI_SSID, WIFI_PASSWORD)
         print('Waiting for connection...', end='')
         # Check if it is connected otherwise wait
         while not wlan.isconnected() and wlan.status() >= 0:
